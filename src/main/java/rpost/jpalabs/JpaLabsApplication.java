832752fd93d4b9246a158c441d41956c5c63c857
Radoslaw Postolowicz <radoslaw.postolowicz@cronn.de>
2024-06-26 23:05:15 +0200

    Initialize spring boot app

---
+++ src/main/java/rpost/jpalabs/JpaLabsApplication.java
@@ -0,0 +1,13 @@
+package rpost.jpalabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaLabsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rpost/jpalabs/JpaLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package rpost.jpalabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
